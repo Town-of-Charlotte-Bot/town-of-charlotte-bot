fd6902d0190e9b781535345e09a5a349347d1e57
KonurPapa <konurpapa@gmail.com>
2018-06-16 16:23:59 -0400

    Update schedule.js

--- schedule.js
+++ schedule.js
@@ -1,4 +1,9 @@
 run = function() {
-    // Do this repeatedly
+    var request = new XMLHttpRequest();
+    request.onload = function() {
+        console.log(this.response);
+    };
+    request.open("GET", "https://www.khanacademy.org/api/internal/discussions/scratchpad/6221507115941888/comments?sort=2", true);
+    request.send();
 };
 run();
