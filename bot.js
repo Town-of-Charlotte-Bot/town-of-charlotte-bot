ffeb60c35f5d0dc89a28101994c1560feaf0b482
KonurPapa <konurpapa@gmail.com>
2018-06-16 16:25:40 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,6 +17,9 @@ Client.on('message', message => {
 setInterval(function() {
     frameCount++;
     var request = new XMLHttpRequest();
+    request.onload = function() {
+        console.log(this.response);
+    };
     request.open("GET", "https://www.khanacademy.org/api/internal/discussions/scratchpad/6221507115941888/comments?sort=2", true);
     request.send();
     
