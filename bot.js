d9a2f1a316ea5c53e1498633759d9d557fcc17eb
KonurPapa <konurpapa@gmail.com>
2018-06-18 17:55:54 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,18 +3,23 @@ const Discord = require("discord.js");
 const Client = new Discord.Client();
 
 const prefix = "//";
-var frameCount = 0;
+//var data = JSON.parse(fs.readFileSync("package.json", "utf8"));
 
 Client.on("ready", () => {
     console.log('I am ready!');
     Client.user.setPresence({ status: 'online', game: { name: 'GAME' } });
 });
 
+//const args = message.content.slice(prefix.length).trim().split(/ +/g);
+//const command = args.shift().toLowerCase();
+
 Client.on("message", message => {
     if (message.content === prefix + /ping/i) {
         message.reply('Pong!');
   	} else if (message.content === prefix + "help") {
         message.reply("\n\n**Help**\n\n`//help` - Displays a list of bot commands\n");
+    } else if (message.content === prefix + "logieboi") {
+        message.reply("***Logie da Bear!***");
     }
 });
 
