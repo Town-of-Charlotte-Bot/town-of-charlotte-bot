1e79f3f556cdacea8aaef74f7e3fe2f33f66ad9c
KonurPapa <konurpapa@gmail.com>
2018-06-18 18:18:23 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,23 +1,23 @@
-// Main
 const Discord = require("discord.js");
 const Client = new Discord.Client();
 
+// Main
 const prefix = "//";
 //var data = JSON.parse(fs.readFileSync("package.json", "utf8"));
 
+// On-load
 Client.on("ready", () => {
-    console.log('I am ready!');
-    Client.user.setPresence({ status: 'online', game: { name: 'GAME' } });
+    console.log('Ready for action!');
 });
 
-//const args = message.content.slice(prefix.length).trim().split(/ +/g);
-//const command = args.shift().toLowerCase();
-
 Client.on("message", message => {
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+    
     if (message.content === prefix + /ping/i) {
         message.reply('Pong!');
   	} else if (message.content === prefix + "help") {
-        message.reply("\n\n__**Help**__\n\n*General Commands*\n`//help` - Lists bot commands\n\n*Game Commands*\n`//game-current` - Shows stats about the current game");
+        message.reply("\n\n__**Help**__\n\n*General Commands*\n`//help` - Lists bot commands\n\n*Game Commands*\n`//game-current` - Shows stats about the current game\n`//game-day` - Gives the day of the current game\n`//game-dead` - Lists the players who are dead in the current game\n`//game-alive` - Lists the players who are alive in the current game");
     } else if (message.content === prefix + "logieboi") {
         message.reply("***Logie da Bear!***");
     }
