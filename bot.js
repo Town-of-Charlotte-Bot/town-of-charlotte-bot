65f376587c0da1138e804623fd0195258f96d756
KonurPapa <konurpapa@gmail.com>
2018-06-18 19:17:26 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,29 +5,32 @@ const Client = new Discord.Client();
 const prefix = "//";
 //var data = JSON.parse(fs.readFileSync("package.json", "utf8"));
 
-// On-load
+// Load
 Client.on("ready", () => {
     console.log('Ready for action!');
 });
 
+// Message
 Client.on("message", message => {
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
     
     if (message.content === prefix + /ping/i) {
-        message.reply('Pong!');
+        message.channel.send('Pong!');
   	} else if (message.content === prefix + "help") {
-        var txt = "\n\n__**Help**__\n";
-        txt += "\n*General Commands*";
+        var txt = "\n\n__** Help **__\n";
+        txt += "\n_General Commands_";
         txt += "\n`//help` - Lists bot commands\n";
-        txt += "\n*Game Commands*\n";
+        txt += "\n_Game Commands_\n";
         txt += "`//game-current` - Shows stats about the current game\n";
         txt += "`//game-day` - Gives the day of the current game\n";
         txt += "`//game-dead` - Lists the players who are dead in the current game\n";
         txt += "`//game-alive` - Lists the players who are alive in the current game";
-        message.reply(txt);
+        message.channel.send(txt);
     } else if (message.content === prefix + "logieboi") {
-        message.reply("***Logie da Bear!***");
+        message.channel.send("***Logie da Bear!***");
+    } else if (message.content === prefix + "konurpapa") {
+        message.channel.send("_Woot!_");
     }
 });
 
