e66b800a047524b1e293d99b440315d1156e8cc5
KonurPapa <konurpapa@gmail.com>
2018-06-18 13:04:15 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,7 +20,7 @@ setInterval(function() {
     request.onload = function() {
         console.log(this.response);
     };
-    request.open("GET", "https://www.khanacademy.org/api/internal/discussions/scratchpad/6221507115941888/comments?sort=2", true);
+    request.open("GET", "https://town-of-charlotte-bot.herokuapp.com", true);
     request.send();
     
     // Borrowed code from a friend
