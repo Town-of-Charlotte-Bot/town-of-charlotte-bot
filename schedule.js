b3ba2d3ed8fd1d898131ba62ea71341eb5cfce25
KonurPapa <konurpapa@gmail.com>
2018-06-18 13:34:32 -0400

    Delete schedule.js

--- schedule.js
+++
@@ -1,9 +0,0 @@
-run = function() {
-    var request = new XMLHttpRequest();
-    request.onload = function() {
-        console.log(this.response);
-    };
-    request.open("GET", "https://www.khanacademy.org/api/internal/discussions/scratchpad/6221507115941888/comments?sort=2", true);
-    request.send();
-};
-run();
