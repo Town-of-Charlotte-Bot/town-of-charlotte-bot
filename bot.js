53940e81b5b0f50df3bd01fb57842db5a764310f
KonurPapa <konurpapa@gmail.com>
2018-06-18 19:21:47 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,14 +18,16 @@ Client.on("message", message => {
     if (message.content === prefix + /ping/i) {
         message.channel.send('Pong!');
   	} else if (message.content === prefix + "help") {
-        var txt = "\n\n__** Help **__\n";
-        txt += "\n_General Commands_";
-        txt += "\n`//help` - Lists bot commands\n";
-        txt += "\n_Game Commands_\n";
-        txt += "`//game-current` - Shows stats about the current game\n";
-        txt += "`//game-day` - Gives the day of the current game\n";
-        txt += "`//game-dead` - Lists the players who are dead in the current game\n";
-        txt += "`//game-alive` - Lists the players who are alive in the current game";
+        // I know this isn't very syntactually good, but it's easier to read
+        var txt = "\n\n__** Help **__\n"
+        + "\n_General Commands_"
+        + "\n`//help` - Lists bot commands\n"
+        + "\n_Game Commands_\n"
+        + "`//game-current` - Shows stats about the current game\n"
+        + "`//game-day` - Gives the day of the current game\n"
+        + ""
+        + "`//game-dead` - Lists the players who are dead in the current game\n"
+        + "`//game-alive` - Lists the players who are alive in the current game";
         message.channel.send(txt);
     } else if (message.content === prefix + "logieboi") {
         message.channel.send("***Logie da Bear!***");
