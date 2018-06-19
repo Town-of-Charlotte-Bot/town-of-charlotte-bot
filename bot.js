ad016df3d698020da9f0ddab44b5e30605a00865
KonurPapa <konurpapa@gmail.com>
2018-06-19 18:49:06 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -91,7 +91,7 @@ client.on("message", message => {
 // When the bot loads
 client.on("ready", () => {
     console.log(`Ready for action! Serving ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} servers.`);
-    client.user.setActivity(`Town of Charlotte ~ ${prefix}`);
+    //client.user.setActivity(`Town of Charlotte ~ ${prefix}`);
 });
 
 // Server joining/leaving
