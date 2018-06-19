cb9a164f857ecf882c5c9c9cb270a0b3b7b9f6d3
KonurPapa <konurpapa@gmail.com>
2018-06-19 06:35:30 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,17 +18,62 @@ Client.on("message", message => {
     if (message.content === prefix + /ping/i) {
         message.channel.send('Pong!');
   	} else if (message.content === prefix + "help") {
-        // I know this isn't very syntactually good, but it's easier to read
-        var txt = "\n\n__** Help **__\n"
-        + "\n_General Commands_"
-        + "\n`//help` - Lists bot commands\n"
-        + "\n_Game Commands_\n"
-        + "`//game-current` - Shows stats about the current game\n"
-        + "`//game-day` - Gives the day of the current game\n"
-        + ""
-        + "`//game-dead` - Lists the players who are dead in the current game\n"
-        + "`//game-alive` - Lists the players who are alive in the current game";
-        message.channel.send(txt);
+        // I know this isn't especially syntactually good, but it makes it easier to read
+        var txt = "__** Help **__\n\n"
+            + "_Command Prefix:_ `" + prefix + "`\n\n"
+            + "_General Commands_\n"
+            + "`help` - Lists bot commands\n\n"
+            + "_Game Commands_\n"
+            + "`game-join` - Join the currently initiated game\n"
+            + "`game-stats` - Show vital-statistics about the current game\n"
+            + "`game-players` - Lists all players in the current game\n"
+            + "`players-dead` - Lists the players who are dead in the current game\n"
+            + "`players-alive` - Lists the players who are alive in the current game\n\n"
+            + "_Gamemaster Commands_\n"
+            + "`game-initiate` - Initiate a new game for players to join\n"
+            + "`game-start` - Start a new game with the players that have joined\n"
+            + "`game-end` - End the current game\n"
+            + "`players-good` - DMs the user a list of all good players in the current game\n"
+            + "`players-evil` - DMs the user a list of all evil players in the current game\n"
+            + "`players-neutral` - DMs the user a list of all neutral players in the current game\n"
+            + "`players-list` - DMs the user a list of all players in the current game and their respective roles";
+        //message.channel.send(txt);
+        message.channel.send({
+            embed: {
+                color: 3447003,
+                author: {
+                    name: "__** Help **__"
+                },
+                title: "List of commands",
+                fields: [
+                    {
+                        name: "General",
+                        value: "`help` - Lists bot commands"
+                    },
+                    {
+                        name: "Game",
+                        value: "`game-join` - Join the currently initiated game\n"
+                            + "`game-stats` - Show vital-statistics about the current game\n"
+                            + "`game-players` - Lists all players in the current game\n"
+                            + "`players-dead` - Lists the players who are dead in the current game\n"
+                            + "`players-alive` - Lists the players who are alive in the current game"
+                    },
+                    {
+                        name: "Gamemaster Commands",
+                        value: "`game-initiate` - Initiate a new game for players to join\n"
+                            + "`game-start` - Start a new game with the players that have joined\n"
+                            + "`game-end` - End the current game\n"
+                            + "`players-good` - DMs the user a list of all good players in the current game\n"
+                            + "`players-evil` - DMs the user a list of all evil players in the current game\n"
+                            + "`players-neutral` - DMs the user a list of all neutral players in the current game\n"
+                            + "`players-list` - DMs the user a list of all players in the current game and their respective roles"
+                    }
+                ],
+                footer: {
+                    text: "Command Prefix: `" + prefix + "`"
+                }
+            }
+        });
     } else if (message.content === prefix + "logieboi") {
         message.channel.send("***Logie da Bear!***");
     } else if (message.content === prefix + "konurpapa") {
