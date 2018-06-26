6f8c17f6f117564aec6ee60c22c80369a46284f3
KonurPapa <konurpapa@gmail.com>
2018-06-25 20:52:22 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -283,7 +283,8 @@ client.on("message", async message => {
                         if (roleType >= 5) roleType = 1;
                         
                         message.channel.send(`_${message.author} has joined the game._`);
-                        message.author.send(`Your role is _${game.players[message.author.username]}_.\n${roles[game.players[message.author.username]].txt}`).catch(error => message.reply(`Failed to perform action: ${error}`));
+                        var name = game.players[message.author.username];
+                        message.author.send(`Your role is _${name}_.\n${roles[name].txt}`).catch(error => message.reply(`Failed to perform action: ${error}`));
                     }
                     if (gameNow && playerIndex !== -1) {
                         message.reply("You have already joined the game.");
