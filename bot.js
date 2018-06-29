b29b654758ae661853aaa631c86104f2fc4a77ef
KonurPapa <konurpapa@gmail.com>
2018-06-29 10:47:35 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -163,9 +163,9 @@ client.on("message", async message => {
                 switch (args[0]) {
                     case "kill":
                         if (args[1] === null) return message.author.send("You must provide the username of your target.");
-                        if (game.alive[message.author.username].abilities.kill === null) return message.author.send("You do not have the ability to kill anyone.");
-                        if (game.alive[args[1]] === null || game.alive[message.author.username].abilities.kill === null) return message.author.send("That player could not be killed. Perhaps you spelled the name incorrectly, or the player is already dead.");
-                        if (game.alive[args[1]] !== null && game.alive[message.author.username].abilities.kill !== null) {
+                        if (game.alive[message.author.username].abilities.kill[0] < 1) return message.author.send("You do not have the ability to kill anyone.");
+                        if (game.alive[args[1]] === null) return message.author.send("That player could not be killed. Perhaps you spelled the name incorrectly, or the player is already dead.");
+                        if (game.alive[args[1]] !== null && game.alive[message.author.username].abilities.kill[0] >= 1) {
                             return client.fetchUser(game.players[args[1]]).then(user => {
                                 user.send(game.alive[message.author.username].abilities.kill[1]);
                             }).catch(error => message.author.send(`Failed to perform action: ${error}`));
