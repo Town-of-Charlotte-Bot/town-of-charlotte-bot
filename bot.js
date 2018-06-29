0df5a9fcdf63058cda093c57c2e9321c22632e40
KonurPapa <konurpapa@gmail.com>
2018-06-29 12:58:20 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -162,8 +162,10 @@ client.on("message", async message => {
             if (listed) {
                 switch (args[0]) {
                     case "kill":
-                        return console.log(roles.good[game.alive[message.author.username]]);
-                        /*if (args[1] === null) return message.author.send("You must provide the username of your target.");
+                        return console.log(roles.good[game.alive[message.author.username]].abilities);
+                        /*
+                        if (game.alive[message.author.username]] === undefined) return message.author.send("You are not playing in the current game.");
+                        if (args[1] === null) return message.author.send("You must provide the username of your target.");
                         if (roles.good[game.alive[message.author.username]].abilities.kill === null) return message.author.send("You do not have the ability to kill anyone.");
                         if (game.alive[args[1]] === null) return message.author.send("That player could not be killed. Perhaps you spelled the name incorrectly, or the player is already dead.");
                         if (game.alive[args[1]] !== null && roles.good[game.alive[message.author.username]].abilities.kill === null) {
