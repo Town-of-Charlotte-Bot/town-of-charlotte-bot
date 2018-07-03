cf3935e11567b76c62fc413da3f0d825df1c315d
KonurPapa <konurpapa@gmail.com>
2018-07-03 11:33:15 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -173,13 +173,20 @@ client.on("message", async message => {
                 */
                 let actionList = ["lock", "block", "kill", "investigate", "heal"];
                 
-                for (var i = 0; i < actionList.length; i++) {
+                if (args[0] === actionList[0]) gameAction(actionList[0]);
+                if (args[0] === actionList[1]) gameAction(actionList[1]);
+                if (args[0] === actionList[2]) gameAction(actionList[2]);
+                if (args[0] === actionList[3]) gameAction(actionList[3]);
+                if (args[0] === actionList[4]) gameAction(actionList[4]);
+                else return message.author.send("That action does not exist. Perhaps you spelled it incorrectly, or the action you were thinking of is different.");
+                
+                /*for (var i = 0; i < actionList.length; i++) {
                     if (args[0] == actionList[i]) {
                         gameAction(actionList[i]);
                     } else {
                         return message.author.send("That action does not exist. Perhaps you spelled it incorrectly, or the action you were thinking of is different.");
                     }
-                }
+                }*/
             } else {
                 message.author.send("You are not allowed to use this command. Perhaps you have been role-blocked, or you are not alive in the current game.");
             }
