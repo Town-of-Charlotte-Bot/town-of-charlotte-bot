7e89af62564040424570c5dd584ec85a1eb3aa70
KonurPapa <konurpapa@gmail.com>
2018-07-11 14:43:01 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -175,9 +175,10 @@ client.on("message", async message => {
                 if (ability === undefined || ability[0] < 1) return message.author.send(`You do not have the ability to ${action} anyone.`);
                 if (game.alive[args[1]] === null) return message.author.send(`That player could not be ${action}ed. Perhaps you spelled the name incorrectly, or the player is dead.`);
                 if (game.alive[args[1]] !== null && ability[0] >= 1) {
-                    game.actions[authorRole.priority][message.author.username] = {};
-                    game.actions[authorRole.priority][message.author.username].action = action;
-                    game.actions[authorRole.priority][message.author.username].target = target;
+                    game.actions[authorRole.priority][message.author.username] = {
+                        action: action,
+                        target: target
+                    };
                     return client.fetchUser(game.alive[args[1]].id).then(user => {
                         message.author.send(`_${args[1]} will be ${action}ed._`);
                         if (ability[1] !== undefined) user.send(ability[1]);
